81ad38463d1ed768d49082b868a5aed613bfcdb4
Chickaboo <131608268+Chickaboo@users.noreply.github.com>
2023-06-29 14:04:13 -0700

    Update search_engine.py

--- search_engine.py
+++ search_engine.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import QUrl, QDir
-from PyQt5.QtWidgets import QMainWindow, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QToolBar, QTabWidget
+from PyQt5.QtWidgets import QMainWindow, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QToolBar, QTabWidget, QFileDialog, QDialog
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from settings_dialog import SettingsDialog
 import json
